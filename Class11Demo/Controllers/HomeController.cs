8090623e0e509f20525e319204d24c69c996edc0
Brian Nelson <41879246+chillbnel@users.noreply.github.com>
2018-10-22 17:31:28 -0700

    Class 11 complete

---
+++ Class11Demo/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Class11Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Class11Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public IActionResult Index(string studentName, int studentAge)
+        {
+            return RedirectToAction("Result", new { studentName, studentAge});
+
+        }
+
+        public ViewResult Result(string studentName, int studentAge)
+        {
+            Student student = new Student();
+            student.Name = studentName;
+            student.age = studentAge;
+            return View(student);
+        }
+
+        public string Cat(string catName="Flash", int catAge=1)
+        {
+            return $"Hello {catName} you are {catAge} years old";
+        }
+
+        public ViewResult Class()
+        {
+            Student student = new Student();
+            student.Name = "Nancy";
+            student.age = 12;
+
+            Student s1 = new Student();
+            s1.Name = "Amanda";
+            s1.age = 51;
+
+            Student s2 = new Student();
+            s2.Name = "Molly";
+            s2.age = 51;
+
+            Student s3 = new Student();
+            s3.Name = "Jeff";
+            s3.age = 32;
+
+            List<Student> myStudents = new List<Student> { student, s1, s2, s3 }; 
+            return View(myStudents);
+        }
+    }
+}
--- Class11Demo/Startup.cs
+++ Class11Demo/Startup.cs
@@ -15,6 +15,7 @@ namespace Class11Demo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +26,17 @@ namespace Class11Demo
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseStaticFiles();
+            app.UseMvc(route =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                route.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=cat}/{id?}");//? means its nullable
             });
+            //app.Run(async (context) =>
+            //{
+                //await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
